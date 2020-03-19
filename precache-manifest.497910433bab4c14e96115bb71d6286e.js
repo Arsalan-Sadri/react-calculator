433143f7fd2f7363457f3cf06fe08b3194d84884
Arsalan Sadri <asadri62@gmail.com>
2020-03-18 21:31:45 -0700

    Updates

---
+++ precache-manifest.497910433bab4c14e96115bb71d6286e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1fa9985e9630c25781d53bf1075248",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "07aee9f4fe7418488fdb",
+    "url": "/react-calculator/static/css/main.5534f026.chunk.css"
+  },
+  {
+    "revision": "b3a4b9c88db5b0bdbbff",
+    "url": "/react-calculator/static/js/2.f1cc9dc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-calculator/static/js/2.f1cc9dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07aee9f4fe7418488fdb",
+    "url": "/react-calculator/static/js/main.af6d6aed.chunk.js"
+  },
+  {
+    "revision": "56a0542d8acbf2d0e659",
+    "url": "/react-calculator/static/js/runtime-main.36858f56.js"
+  }
+]);
